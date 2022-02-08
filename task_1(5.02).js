c25616d7f9fbf4522fed7ee696bf8dc244e3478b
alexandr50 <dmitrymaslov2016@mail.ru>
2022-02-08 22:05:27 +0300

    Homework for lesson 3

---
+++ task_1(5.02).js
@@ -0,0 +1,14 @@
+let nums = 0
+while (nums <= 100) {
+    let check = true;
+    for (let i = 2; i < nums; i++) {
+        if (nums % i === 0) {
+            check = false;
+            break;
+        }
+    }
+
+    if (check) console.log(nums);
+    nums++;
+
+}
